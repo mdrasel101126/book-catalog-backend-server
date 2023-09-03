1526def1707d7b28404fa7b6022d49cdc2da6b6c
mdrasel101126 <mdrasel101126@gmail.com>
2023-09-03 12:57:30 +0600

    authentication added to user service

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
 
 const router = Router();
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidation.updateUser),
   UserController.updateUser
 );
-router.delete('/:id', UserController.deleteUser);
-router.get('/:id', UserController.getSingleUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 
 export const UserRoutes = router;
