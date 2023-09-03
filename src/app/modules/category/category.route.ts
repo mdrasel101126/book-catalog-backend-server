1a1650a78d756a524b308767fa24e045dfb6d385
mdrasel101126 <mdrasel101126@gmail.com>
2023-09-03 13:10:39 +0600

    authentication added to category sevice

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
 import { CategoryValidation } from './category.validation';
@@ -7,15 +9,21 @@ const router = Router();
 
 router.post(
   '/create-category',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(CategoryValidation.createCategory),
   CategoryController.createCategory
 );
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(CategoryValidation.updateCategory),
   CategoryController.updateCategory
 );
-router.delete('/:id', CategoryController.deleteCategory);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.deleteCategory
+);
 router.get('/', CategoryController.getAllCategory);
 router.get('/:id', CategoryController.getSingleCategory);
 
