95b456911e99252b5077fc444b6faa2518320e76
mdrasel101126 <mdrasel101126@gmail.com>
2023-09-03 13:29:58 +0600

    authentication added to book service

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BookController } from './book.controller';
 import { BookValidation } from './book.validation';
@@ -7,15 +9,17 @@ const router = Router();
 
 router.post(
   '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidation.createBook),
   BookController.createBook
 );
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidation.updateBook),
   BookController.updateBook
 );
-router.delete('/:id', BookController.deleteBook);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), BookController.deleteBook);
 router.get('/', BookController.getAllBooks);
 router.get('/:categoryId/category', BookController.getBooksByCategory);
 router.get('/:id', BookController.getSingleBookById);
